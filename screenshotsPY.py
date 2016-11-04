cd8500843c73fcc6e1eeebcae40bf36e31d97ae8
mcmoonstruck <micheleacostello@gmail.com>
2016-11-04 16:45:22 -0700

    Drastically reorganized screenshotsPY.py file

--- screenshotsPY.py
+++ screenshotsPY.py
@@ -11,189 +11,199 @@ import sys
 from requests import Session
 from time import sleep
 
-### uncomment if you want to use multiple account logins
-#userUsername = "yourusename"
-#userPassword = "yourpassword"
 
-adminUsername = "yourusernamehere"
-adminPassword = "yourpasswordhere"
-adminAPItoken = "yourAPItokenhere"
-
+### hard-coded information ###
 # hard-coded number of images that should be processed in one run
 chunksize = 35
 
 # hard-coded info used to run environments and for initial account login by phantomjs script
 baseURL = "https://cloud.skytap.com"
-res_env = "/configurations"
-res_acc = "/account"
 
-# lists that will be populated by the script
-userScreenshots = []
-adminScreenshots = []
-envstoRun = []
-okaysysargs = []
-
-
-##### functions related to verifying system arguments #####
-# function to generate a list of valid system arguments (system argument is used to determine relevant screenshots)
-def makelistofValidArgs():
-    for screenshot in allscreenshots:
-        if screenshot['cliname'] not in okaysysargs :
-            okaysysargs.append(screenshot['cliname'])
-        if screenshot['group'] not in okaysysargs:
-            okaysysargs.append(screenshot['group'])
-
-# check if an invalid system argument was provided; if not, exit the script
-def checkforValidArg():
-    # if a flag was not provided by the user or an invalid option was provided, print a list of valid arguments
-    if (len(sys.argv) < 2) or (sys.argv[1] not in okaysysargs):
-        print "Enter a screenshot list to generate. Choose from:"
-        for p in okaysysargs: print p 
-        sys.exit()
-
-##### functions related to starting and stopping environments ##### 
-# function to generate a list of environments that need to be run
-def makelistofEnvstoRun():
-    if screenshot['run'] and screenshot['run'] not in envstoRun:
-        envstoRun.append(screenshot['run'])
-
-#create API session
-def create_session(username, key):
-    session = Session()
-    session.auth = (username, key)
-    session.headers = {"Accept": "application/json",
-                        "Content-type": "application/json"}
-    return session
-
-# use API session to run or suspend environments
-def editenvrunstate(runstate, user, api_key):
-    # start and test the API session
-    api_session = create_session(user, api_key)
-    auth_test = api_session.get(baseURL + res_acc) # Try a get against account
-    authed = False if auth_test.status_code == 401 else True
-    if not authed:
-        print('Credentials incorrect, response code: {0}'.format(auth_test.status_code))
-        print('Check your username/api key and try again')
-        sys.exit()
+class SkytapUser(object):
+    _registry = []
     
-    # manage runstate of each environment
-    params = {'runstate': runstate}
-    for env in envstoRun:
-        statusChange = 0
-        get_url = "{0}{1}/{2}".format(baseURL, res_env, env)
-        put_url = "{0}.json".format(get_url)
-       
-        # function to change the environment runstate
-        def changeRunstate():
-            if statusChange < 1:
-                response = api_session.put(put_url, params=params)
-                json_output = json.loads(response.text)  
+    def __init__(self, name, username, password, apikey):
+        # create a new user
+        self._registry.append(self)
+        self.name = name
+        self.username = username
+        self.password = password
+        self.apikey = apikey
+        self.screenshotlist = []
+
+
+class ProcessCSV(object):
+    def __init__(self, csvname, listfromcsv): 
+        self.csvname = csvname 
+        self.listfromcsv = []        
+        with open(self.csvname, 'rU') as f:
+            self.listfromcsv = [{k: str(v) for k, v in row.items()}
+                for row in csv.DictReader(f, skipinitialspace=True)]
+    
+    # function to read the list data and filter it into smaller lists to be used by the rest of the script
+    def filter_screenshots(self, filter_name, envs_to_run):
         
-        # funciton to check the environment runstate.        
-        def checkRunstate():
-            response = api_session.get(get_url)
+        ### internal functions used by filter_screenshots ###
+        # function to add environment to envs_to_run list
+        def add_to_envs_to_run(env):
+            if len(env) > 0 and env not in envs_to_run:
+                envs_to_run.append(env)
+                return envs_to_run
+        
+        # function to add screenshot to appropriate screenshot list    
+        def add_to_screenshots_to_take(screenshot):
+            screenshot_user_name = screenshot['permissionLevel']
+            for user in SkytapUser._registry:
+                if screenshot_user_name == user.name:
+                    user.screenshotlist.append(screenshot)
+            return user.screenshotlist
+                
+        for screenshot in self.listfromcsv:
+            if filter_name in (screenshot['cliname'], screenshot['group']):
+                add_to_envs_to_run(screenshot['run'])
+                add_to_screenshots_to_take(screenshot)
+        return envs_to_run
+
+   
+class RestClient(object):
+    def __init__(self, user, apikey): 
+        self.user = user 
+        self.apikey = apikey
+    
+    #function to edit the environment runstate
+    def check_and_edit_envs(self, envs_to_run, runstate):
+        
+        ### functions used by check_and_edit_envs ###
+        # function to check the environment runstate        
+        def in_desired_runstate(runstate, get_url, session):
+            response = session.get(get_url)
             json_output = json.loads(response.text)
-            # if environment is in desired runstate, move on.
+            # if environment is in desired runstate, return true
             if json_output['runstate'] == runstate:
-                print('Environment ' + env + ' is ' + runstate + '.')        
-            # if environment is not in desired runstate, do other things
+                print('Environment ' + env + ' is ' + runstate + '.')
+                return True
+        
+        # function to change the environment runstate
+        def change_runstate(runstate, put_url, session):
+            params = {'runstate': runstate}
+            response = session.put(put_url, params=params)
+            json_output = json.loads(response.text)
+            if response.status_code == 200:
+                print('Environment ' + env + ' is switching to ' + runstate + '.')
+                recheck_runstate(runstate, put_url, session)
             else:
-                #change the environment runstate. this should only happen once per env.
-                changeRunstate()
-                #if request was successful but environment is still busy, keep checking the runstate until the environment is in desired state
-                if response.status_code == 200:
-                    statusChange = 1
-                    print('Environment ' + env + ' is switching to ' + runstate + '.')
-                    sleep(10)
-                    checkRunstate()
-               #if the request was not successful, print the error message.
-                else:
-                    print('Error: Environment ' + env + ' is NOT ' + runstate + '.')
-                    print('Error code: {0}'.format(response.status_code))    
-        checkRunstate()       
+                print('Error: Environment ' + env + ' is NOT ' + runstate + '.')
+                print('Error code: {0}'.format(response.status_code))
+      
+        # function to continually recheck the environment until it is in the desired runstate. 
+        def recheck_runstate(runstate, get_url, session):
+            sleep(10)
+            if in_desired_runstate(runstate, get_url, session):
+                return
+            else:
+                recheck_runstate(runstate, get_url, session)
+                print 'Environment ' + env + ' is switching to ' + runstate + '.'
+                
+        ### start check_and_edit_envs here ###        
+        
+        if len(envs_to_run) > 0:  
+            print "Checking the runstate of " + str(len(envs_to_run)) + " environment(s)"
+            
+            # start the API session
+            session = Session()
+            session.auth = (self.user, self.apikey)
+            session.headers = {"Accept": "application/json",
+                                "Content-type": "application/json"}
+            
+            # check the credentials 
+            auth_test = session.get(baseURL + "/account") # Try a get against account
+            authed = False if auth_test.status_code == 401 else True
+            
+            # exit if the credentials are not valid, continue if they are
+            if not authed:
+                print('API credentials are incorrect, response code: {0}'.format(auth_test.status_code))
+                print('Check the admin username and API key and try again')
+                sys.exit()
+            else:
+                # loop through envs. check if they are already in the desired runstate; edit them if they are not
+                for env in envs_to_run:
+                    get_url = "{0}{1}/{2}".format(baseURL, "/configurations", env)
+                    put_url = "{0}.json".format(get_url)
+                    if in_desired_runstate(runstate, get_url, session):
+                        return
+                    else:
+                        change_runstate(runstate, put_url, session)    
 
-# run environments
-def runEnvironments():
-    # run the environments as the admin
-    if len(envstoRun) > 0:  
-        # print the list of environments to run
-        print "List of envs to run: "
-        for envs in envstoRun: print envs
-        editenvrunstate("running", adminUsername, adminAPItoken)
-    else: print "No environments to run."
-
-# suspend the environments 
-def suspendEnvironments():
-    if len(envstoRun) > 0:  
-        editenvrunstate("suspended", adminUsername, adminAPItoken)
+def generate_screenshots(user, filter_name):
+    if len(user.screenshotlist) > chunksize:
+        run_in_chunks(user.screenshotlist)
     else:
-        print "No environments to suspend."
+        switch_to_PhantomJS(filter_name, user.screenshotlist, user.username, user.password)
 
-##### functions related to handing off relevant screenshot lists to phantomJS script ##### 
-# create a more specific .json file with a list of screenshots that phantomJS must generate.
-def createScreenshotFile(listname):
-    # create a text file with the CLI flag name
-    fname = os.path.join(sys.argv[1]+".json")  
-    # add any relevant screenshots to that json file
-    with open(fname, 'w') as outfile:
-        json.dump(listname, outfile)
-
-# pass this file info and the user credentials along to phantomJS.
-def startPJS(username, password):
-    # use the CLI to run the phantomJS script. pass along arguments from this script
-    newsyscommand = "phantomjs screenshotsJS.js " + baseURL + " " + username + " " + password + " " + sys.argv[1]
-    print "SWITCH TO PHANTOM JS________"
-    os.system(newsyscommand)
-
-def breakintochunks(listname, uname, pword):
-    chunks = [listname[x:x+chunksize] for x in xrange(0, len(listname), chunksize)]
+def run_in_chunks(listtobreakdown):
+    chunks = [listtobreakdown[x:x+chunksize] for x in xrange(0, len(listtobreakdown), chunksize)]
     print "breaking into " + str(len(chunks)) + " batches"
     chunknum = 0
     for chunk in chunks:
         print "starting batch #" + str(chunknum)
-        createScreenshotFile(chunk)
-        startPJS(uname, pword)
+        switch_to_PhantomJS(chunk, user.username, user.password)
         chunknum = chunknum + 1
         print "pausing for 20 seconds"
         sleep(20)
 
-# pass along lists to PhantomJS
-def switchtoPJS():
-    #if there are any screenshots to generate with user permissions, pass that along to phantomjs
-    if len(userScreenshots) > 0:
-        if len(userScreenshots) > chunksize:
-            breakintochunks(userScreenshots, userUsername, userPassword)
-        else:
-            createScreenshotFile(userScreenshots)
-            startPJS(userUsername, userPassword)   
-    #if there are any screenshots to generate with admin permissions, pass that along to phantomjs
-    if len(adminScreenshots) > 0:
-        if len(adminScreenshots) > chunksize:
-            breakintochunks(adminScreenshots, adminUsername, adminPassword)
-        else:
-            createScreenshotFile(adminScreenshots)
-            startPJS(adminUsername, adminPassword) 
-
-##### !!!!!!!!!!!! #####
-##### start script #####
-with open('allscreenshots.csv', 'rU') as f:
-    allscreenshots = [{k: str(v) for k, v in row.items()}
-        for row in csv.DictReader(f, skipinitialspace=True)]
-makelistofValidArgs()
-checkforValidArg()
-for screenshot in allscreenshots:
-    if (sys.argv[1] == screenshot['cliname'] or sys.argv[1] == screenshot['group']):
-        makelistofEnvstoRun()
-        if screenshot['permissionLevel'] == "user":
-            userScreenshots.append(screenshot)
-        if screenshot['permissionLevel'] == "admin":
-            adminScreenshots.append(screenshot)
-runEnvironments()
-switchtoPJS()
-suspendEnvironments()
+#create a JSON file; pass this and the user credentials along to phantomJS.
+def switch_to_PhantomJS(filter_name, listname, username, password):
+    #create a text file with the CLI flag name
+    fname = os.path.join(filter_name+".json")  
+    #add any relevant screenshots to that json file
+    with open(fname, 'w') as outfile:
+        json.dump(listname, outfile)
     
-exit
-#_______    
+    #use the CLI to run the phantomJS script
+    newsyscommand = "phantomjs screenshotsJS.js " + baseURL + " " + username + " " + password + " " + filter_name
+    print "SWITCH TO PHANTOM JS________"
+    os.system(newsyscommand)
+
+def main():
+    # read in the system argument and store it as a variable; exit if an argument is not provided    
+    if (len(sys.argv) < 2):
+        print "Please enter a 'cliname' or 'group' attribute from the .csv file"
+        exit()
+    else:
+        filter_name = sys.argv[1]
+
+    # add users the phantomJS script can use to log into Skytap as
+    # uncomment lines as needed and replace "instertusername", "insertpassword", "") with the correct username, password, and API token.
+    # user = SkytapUser("user", "insertusername", "insertpassword", "")
+    admin = SkytapUser("admin", "instertusername", "insertpassword", "")
+    
+    # read from .csv file to create a list of all possible screenshots
+    allscreenshots = []
+    allscreenshots = ProcessCSV('allscreenshots.csv', allscreenshots)
+    
+    #filter down screenshots based on system argument filter name
+    envs_to_run = []
+    envs_to_run = allscreenshots.filter_screenshots(filter_name, envs_to_run)
+    
+    # run environments as admin, if needed
+    if envs_to_run > 0:
+        rest_client = RestClient(admin.username, admin.apikey)
+        rest_client.check_and_edit_envs(envs_to_run, "running")
+    
+    # for each skytap user, use PhantomJs to log into Skytap as the user and take a series of screenshots
+    for user in SkytapUser._registry:
+        if len(user.screenshotlist) > 0:
+            generate_screenshots(user, filter_name)
+
+    # suspend environments as admin, if needed        
+    if envs_to_run > 0:
+        rest_client.check_and_edit_envs(envs_to_run, "suspended")
+    
+    exit
+
+if __name__ == "__main__":
+    main()
+    
         
             
     
